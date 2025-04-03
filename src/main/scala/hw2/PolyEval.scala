c79be5c5370f06a41f8a8e50f85584958b8fd795
Emanuele Parisi <emanuele.parisi@bsc.es>
2025-04-03 19:37:08 +0200

    Add solution to problem 2

--- src/main/scala/hw2/PolyEval.scala
+++ src/main/scala/hw2/PolyEval.scala
@@ -9,10 +9,20 @@ import chisel3._
   * @param width : the width of x
   */
 class PolyEval(coefs: Seq[Int], width: Int) extends Module {
+  require (coefs.length > 0, "coefs must be non-empty")
+
+  def helper(c: Seq[Int]): UInt = {
+    if (c.isEmpty) {
+      0.U
+    } else {
+      c.head.U + io.x * helper(c.drop(1))
+    }
+  }
+  
   val io = IO(new Bundle {
     val x      = Input(UInt(width.W))
     val out    = Output(UInt())
   })
 
-  ???
+  io.out := helper(coefs)
 }
--- src/test/scala/hw2/PolyEvalTester.scala
+++ src/test/scala/hw2/PolyEvalTester.scala
@@ -1,15 +1,26 @@
 package hw2
 
+import scala.math._
+
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class PolyEvalTester extends AnyFlatSpec with ChiselScalatestTester {
-  val width = ???
+  val width = 8
   def testPolyEvalOut(n: Int): Unit = {
-    val coefs = ???
+    val coefs = Seq.fill(n)(1)
     test(new PolyEval(coefs, width)) { dut =>
-      ???
+      def f(x: BigInt): BigInt = (x.pow(coefs.length) - 1) / (x - 1)
+
+      dut.io.x.poke(2.U)
+      dut.io.out.expect(f(2).U)
+
+      dut.io.x.poke(5.U)
+      dut.io.out.expect(f(5).U)
+
+      dut.io.x.poke(13.U)
+      dut.io.out.expect(f(13).U)
     }
   }
 
